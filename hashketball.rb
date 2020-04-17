8ea1d5a53d3f315bf010507605a69d33838c4fdb
Ian Rosen <irosen419@gmail.com>
2020-04-17 18:25:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,61 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{:player_name => "Jeff Adrian",
+      :players => [{:player_name => "Alan Anderson",
+                    :number => 0,
+                    :shoes => 16,
+                    :points => 22,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks => 1
+                    },
+                    {:player_name => "Reggie Evans",
+                    :number => 30,
+                    :shoes => 14,
+                    :points => 12,
+                    :rebounds => 4,
+                    :assists => 7,
+                    :steals => 22,
+                    :blocks => 15,
+                    :slam_dunks => 10
+                    },
+                    {:player_name => "DeSagna Biop",
+                    :number => 2,
+                    :shoes => 14,
+                    :points => 24,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 4,
+                    :blocks => 5,
+                    :slam_dunks => 5
+                    },
+                    {:player_name => "Ben Gordon",
+                    :number => 8,
+                    :shoes => 15,
+                    :points => 33,
+                    :rebounds => 3,
+                    :assists => 2,
+                    :steals => 1,
+                    :blocks => 1,
+                    :slam_dunks => 0
+                    },
+                    {:player_name => "Kemba Walker",
+                    :number => 33,
+                    :shoes => 15,
+                    :points => 6,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 7,
+                    :blocks => 5,
+                    :slam_dunks => 12
+                    }]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [{:player_name => "Jeff Adrien",
                     :number => 4,
                     :shoes => 18,
                     :points => 10,
@@ -52,23 +106,7 @@ def game_hash
                     :steals => 7,
                     :blocks => 5,
                     :slam_dunks => 12
-                    }
-                    
-      ]
-    },
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"],
-      :players => [:player_name => nil,
-                   :number => nil,
-                   :shoes => nil,
-                   :points => nil,
-                   :rebounds => nil,
-                   :assists => nil,
-                   :steals => nil,
-                   :blocks => nil,
-                   :slam_dunks => nil
-                   ]
+                    }]
     },
   }
 end
