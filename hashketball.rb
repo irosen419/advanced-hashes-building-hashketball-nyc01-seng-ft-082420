b66638dd1a392b8dc5ac66668c29a824227ef797
Ian Rosen <irosen419@gmail.com>
2020-04-17 18:12:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,29 +3,29 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [:player_name => ,
-                   :number => ,
-                   :shoes => ,
-                   :points => ,
-                   :rebounds => ,
-                   :assists => ,
-                   :steals => ,
-                   :blocks => ,
-                   :slam_dunks => 
+      :players => [:player_name => nil,
+                   :number => nil,
+                   :shoes => nil,
+                   :points => nil,
+                   :rebounds => nil,
+                   :assists => nil,
+                   :steals => nil,
+                   :blocks => nil,
+                   :slam_dunks => nil
                    ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [:player_name => ,
-                   :number => ,
-                   :shoes => ,
-                   :points => ,
-                   :rebounds => ,
-                   :assists => ,
-                   :steals => ,
-                   :blocks => ,
-                   :slam_dunks => 
+      :players => [:player_name => nil,
+                   :number => nil,
+                   :shoes => nil,
+                   :points => nil,
+                   :rebounds => nil,
+                   :assists => nil,
+                   :steals => nil,
+                   :blocks => nil,
+                   :slam_dunks => nil
                    ]
     },
   }
