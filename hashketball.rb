44a6b0ae29d7f0584512974cc8c8b6c34586afcd
Ian Rosen <irosen419@gmail.com>
2020-04-17 18:18:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,7 +32,18 @@ def game_hash
                     :steals => nil,
                     :blocks => nil,
                     :slam_dunks => nil
-                    }
+                    },
+                    {:player_name => nil,
+                    :number => nil,
+                    :shoes => nil,
+                    :points => nil,
+                    :rebounds => nil,
+                    :assists => nil,
+                    :steals => nil,
+                    :blocks => nil,
+                    :slam_dunks => nil
+                    },
+                    
       ]
     },
     :away => {
