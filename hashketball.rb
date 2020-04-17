b78f4ab8c779956798d9b02839f1458e48a9aca3
Ian Rosen <irosen419@gmail.com>
2020-04-17 18:31:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,10 +18,10 @@ def game_hash
                     :shoe => 14,
                     :points => 12,
                     :rebounds => 12,
-                    :assists => 7,
-                    :steals => 22,
-                    :blocks => 15,
-                    :slam_dunks => 10
+                    :assists => 12,
+                    :steals => 12,
+                    :blocks => 12,
+                    :slam_dunks => 7
                     },
                     {:player_name => "Brook Lopez",
                     :number => 11,
